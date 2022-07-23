28ff453f83be5e5839cb6e8fcbac63c7e65556f3
Alberto Beltran <albertobeltran@protonmail.com>
2022-07-22 21:47:25 -0500

    added logic to close modal button

--- src/components/CloseButton/CloseButton.jsx
+++ src/components/CloseButton/CloseButton.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 import styles from './CloseButton.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const CloseButton = () => {
+
+    const navigate = useNavigate();
+    const handleOpenModal = () => {
+        navigate('/home');
+    }
+
+
     return (
-        <AiOutlineCloseCircle className={styles.icon} />
+        <AiOutlineCloseCircle className={styles.icon} onClick={handleOpenModal} />
     )
 }
 
